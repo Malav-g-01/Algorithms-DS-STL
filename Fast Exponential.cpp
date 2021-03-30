46eae9eee1b0e2cdac9f118b10f9375bcecb6db5
Malav-g-01 <76492477+Malav-g-01@users.noreply.github.com>
2021-03-30 15:18:13 +0530

    Create Fast Exponential.cpp

---
+++ Fast Exponential.cpp
@@ -0,0 +1,33 @@
+/*
+        Author :- Malav Gajjar
+*/
+#include<bits/stdc++.h>
+#define ll long long
+
+using namespce std;
+
+ll power(ll a, ll b)
+{
+        if(b == 0)
+                return 1;
+        
+        if(b == 1)
+                return a;
+        
+        ll tmp = power(a, b/2);
+        ll result = tmp*tmp;
+        
+        if(b%2 == 1)
+                result *= a;
+        
+        return result;
+}
+                
+
+int main()
+{
+        ll a, b;
+        cin >> a >> b; // a = base, b = exponent
+        
+        cout << power(a, b);
+}
