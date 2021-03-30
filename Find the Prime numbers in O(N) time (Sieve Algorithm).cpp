1120f7d640dcf9afc283d6411f7740a4c99e145a
Malav-g-01 <76492477+Malav-g-01@users.noreply.github.com>
2021-03-30 15:03:30 +0530

    Update Find the Prime numbers in O(N) time (Sieve Algorithm).cpp

--- Find the Prime numbers in O(N) time (Sieve Algorithm).cpp
+++ Find the Prime numbers in O(N) time (Sieve Algorithm).cpp
@@ -7,7 +7,36 @@
 
 using namespace std;
 
+const int N = 1e7;
+
+ll prime[N] = {0};
+
+void sieve()
+{
+	for(ll i=2; i<=N; i++)
+	{
+		if(!prime[i])
+		{
+			for(ll j=i*i; j<=N; j++)
+				prime[j] = 1;
+		}
+	}
+}
+
 int main()
 {
-
+	ll n;
+	cin >> n;
+	sieve();
+	ll i, j, k;
+	ll ans=0;
+	
+	if(n>=2)
+	{
+		for(i=2; i<=n; i++)
+			if(!prime[i])
+				ans += i;
+	}
+	
+	cout << ans;
 }
